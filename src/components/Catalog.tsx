f87fd334fd8d8c5813c7b99d8a4b1ec630aed074
Gustavo Harff <gustavo.harff@gmail.com>
2022-03-24 18:54:51 -0300

    feat: List catalog products

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,9 +1,28 @@
-import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+
+import { api } from '../services/api'
+
+import type { Product } from '../store/modules/cart/types'
 
 export function Catalog() {
-  const catalog = useSelector(state => state)
+  const [catalog, setCatalog] = useState<Product[]>([])
 
-  console.log(catalog)
+  useEffect(() => {
+    api.get('/products').then(response => {
+      setCatalog(response.data)
+    })
+  }, [])
 
-  return <h1>Catalog</h1>
+  return (
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {' -'}
+          <span>{product.price}</span> <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
+  )
 }
